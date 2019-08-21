bab991bc4eb13f51176ae081e7a277a006c9e1c8
Trevor Sundberg <tsundberg@s2.systems>
2019-08-21 00:04:24 -0700

    Fixed tests

--- test/test.ts
+++ test/test.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-sync */
-import {JavaScript, Task, TaskRoot, Utility} from "../src/main";
+import {JavaScript, Task, TaskRoot} from "../src/main";
 import assert from "assert";
 import fs from "fs";
 
@@ -12,30 +12,8 @@ import fs from "fs";
   await root.run();
   fs.writeFileSync("/test2.txt", "hello2");
   fs.writeFileSync("/test3.txt", "hello3");
-  fs.mkdirSync("/dir");
-  fs.mkdirSync("/dir/test/");
-  fs.writeFileSync("/dir/test/file.txt", "hello3");
   assert.deepEqual(fs.readFileSync("/test1.txt", "utf8"), "hello1");
   assert.deepEqual(fs.readFileSync("/test2.txt", "utf8"), "hello2");
   assert.deepEqual(fs.readFileSync("/test3.txt", "utf8"), "hello3");
-
-  assert.deepEqual(Utility.fsMatch("test1.txt", "path"), ["/test1.txt"]);
-  assert.deepEqual(Utility.fsMatch("/test1.txt", "path"), ["/test1.txt"]);
-  assert.deepEqual(Utility.fsMatch("*1*", "glob"), ["/test1.txt"]);
-  assert.deepEqual(Utility.fsMatch("/dir/test/*", "glob"), ["/dir/test/file.txt"]);
-  assert.deepEqual(Utility.fsMatch("/dir/**", "glob"), ["/dir/test/file.txt"]);
-  assert.deepEqual(Utility.fsMatch("/*.txt", "glob"), [
-    "/test1.txt",
-    "/test2.txt",
-    "/test3.txt"
-  ]);
-  assert.deepEqual(Utility.fsMatch("1", "regex"), ["/test1.txt"]);
-  assert.deepEqual(Utility.fsMatch(".*file.*", "regex"), ["/dir/test/file.txt"]);
-  assert.deepEqual(Utility.fsMatch("/dir", "regex"), ["/dir/test/file.txt"]);
-  assert.deepEqual(Utility.fsMatch("^/[^\\/]*\\.txt", "regex"), [
-    "/test1.txt",
-    "/test2.txt",
-    "/test3.txt"
-  ]);
   console.log("Completed");
 })();
